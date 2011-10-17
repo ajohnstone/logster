07af60dea88d4a36fa3e92c73d3c3935d80ddc44
Andrew Johnstone <andrew@everlution.com>
2011-10-17 13:44:52 +0100

    Cron

--- cron.sh
+++ cron.sh
@@ -1,7 +1,10 @@
-/usr/sbin/logster -D --metric-prefix="servers.`hostname`.apache.error_log" --output=graphite --graphite-host=graphite.everlution.com:2003 ApacheErrorLogster /var/log/apache2/error.log
+GRAPHITE_HOST="graphite.example.com:2003";
+
+sanitized_name_host=$(echo `hostname` | sed 's/[.]/_/g;');
+/usr/sbin/logster -D --metric-prefix="servers.${sanitized_name_host}.apache.error_log" --output=graphite --graphite-host=$GRAPHITE_HOST ApacheErrorLogster /var/log/apache2/error.log
 
 for i in $(ls /var/www/vhosts/); do
     sanitized_name=$(echo $i | sed 's/[.]/_/g;');
-    /usr/sbin/logster -D --metric-prefix="servers.`hostname`.project.${sanitized_name}.apache.error_log" --output=graphite --graphite-host=graphite.example.com:2003 ApacheErrorLogster /var/www/vhosts/$i/statistics/logs/error.log
+    /usr/sbin/logster -D --metric-prefix="servers.${sanitized_name_host}.project.${sanitized_name}.apache.error_log" --output=graphite --graphite-host=$GRAPHITE_HOST ApacheErrorLogster /var/www/vhosts/$i/statistics/logs/error.log
 done;
 
